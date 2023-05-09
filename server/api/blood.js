125f6da692e841f23fd9f3668e70b00ac5cb9919
hossamemamo <78453559+hossamemamo@users.noreply.github.com>
2023-05-09 12:32:59 +0300

    tested fetching data to table in blood.js

--- server/api/blood.js
+++ server/api/blood.js
@@ -3,4 +3,28 @@ const router=express.Router();
 
 const Blood =require('./../models/blood');
 
+router.get('/bloods', (req, res) => {
+    Blood.find()
+      .then((data) => {
+        res.json({
+          status: "SUCCESS",
+          message: "Retrieved all users",
+          data: data
+        });
+      })
+      .catch((err) => {
+        res.json({
+          status: "FAILED",
+          message: "An error occurred while retrieving users",
+          error: err
+        });
+      });
+  });
+  
+
+
+
+
+
+
 module.exports = router;
